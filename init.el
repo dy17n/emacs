3d6e4d199c2b5a1948dbd5a1c96044dbbf48032c
dylan <krecker@keemail.me>
2025-01-16 17:40:02 -0600

    allah

--- init.el
+++ init.el
@@ -30,3 +30,18 @@
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
 (set-frame-font "Terminus 12" nil t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("a68ec832444ed19b83703c829e60222c9cfad7186b7aea5fd794b79be54146e6" default))
+ '(package-selected-packages
+   '(magit centaur-tabs vim-tab-bar mood-line modus-themes doom-modeline adwaita-dark-theme)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
