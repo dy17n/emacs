54001a810a34f6e78c97ce5a3a2c1177d63485bc
dylan <krecker@keemail.me>
2025-01-16 18:11:47 -0600

    jews

--- init.el
+++ init.el
@@ -25,23 +25,18 @@
 
 (use-package magit)
 
+(use-package enlight
+  :init
+  (setopt initial-buffer-choice 'enlight))
   
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
 (set-frame-font "Terminus 12" nil t)
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("a68ec832444ed19b83703c829e60222c9cfad7186b7aea5fd794b79be54146e6" default))
  '(package-selected-packages
-   '(magit centaur-tabs vim-tab-bar mood-line modus-themes doom-modeline adwaita-dark-theme)))
+   '(enlight magit centaur-tabs vim-tab-bar mood-line modus-themes doom-modeline adwaita-dark-theme)))
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  )
