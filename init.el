50d4fd19cc63c343cf70f7ab725542e380aa38fd
dylan <krecker@keemail.me>
2025-01-17 00:11:19 -0600

    company-mode

--- init.el
+++ init.el
@@ -22,14 +22,26 @@
 (use-package enlight
   :init
   (setopt initial-buffer-choice 'enlight))
+
+(use-package company)
+(add-hook 'after-init-hook 'global-company-mode)
   
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
 (set-frame-font "Terminus 12" nil t)
 (custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("a68ec832444ed19b83703c829e60222c9cfad7186b7aea5fd794b79be54146e6" default))
  '(package-selected-packages
-   '(enlight magit centaur-tabs vim-tab-bar mood-line modus-themes doom-modeline adwaita-dark-theme)))
-(custom-set-faces)
+   '(company enlight magit centaur-tabs vim-tab-bar mood-line modus-themes doom-modeline adwaita-dark-theme)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
